011bc8a13de86e76f1b7f39bf7b2f3955b2f1864
pili-goenaga <mgoenagaarrambide@itba.edu.ar>
2025-08-14 20:58:59 -0300

    prueba

---
+++ teorica.js
@@ -0,0 +1,53 @@
+let nombre = "pilar" ;
+let edad = 20 ; 
+let profesor = true;
+edad = edad + 1;
+edad +=1;
+// const PI = 3.14;
+// PI+=1
+// console.log(PI);
+
+const ARRAY1 = [1,2,3];
+cosole.log(ARRAY1);
+ARRAY1.push(4);
+
+// objeto literal 
+let alumno = {
+    nombre: "pilar",
+    apellido: "goenaga",
+    edad: 20,
+    dueda: false,
+}
+console.log(alumno);
+console.log(alumno.nombre);
+
+// para buscar a alg en particular 
+// consule.log(CLASE)
+//console.log(CLaASE[0].nombre); la teca 
+// let clase = [
+//     {
+//         nombre: "pilar",
+//         apellido: "goenaga",
+//         edad: 20,
+//         deuda: false,
+//     },
+//     {
+//         nombre: "maria",
+//         apellido: "lopez",
+//         edad: 22,
+//         deuda: true,
+//     },
+//     {
+//         nombre: "juan",
+//         apellido: "perez",
+//         edad: 21,
+//         deuda: false,
+//     }
+// ]; ejemplo de array de objetos\
+
+console.log("perro" && "gato");
+console.log("perro" || "gato");
+
+console.log(Number("2"));
+let numeroEntero = 2;
+console.log(numero)
