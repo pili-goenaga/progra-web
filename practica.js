e1c80f809d58f3cf2b91e1987bdec57175197887
pili-goenaga <mgoenagaarrambide@itba.edu.ar>
2025-08-28 20:24:33 -0300

    prueba html

---
+++ practica.js
@@ -0,0 +1,7 @@
+// 1. Ejercicio 1: Declaración de Variables
+// Declara tres variables: una para almacenar tu ciudad, otra para almacenar tu país, y otra para almacenar
+//  tu edad de nacimiento. Imprime los valores en la consola y después intenta cambiar los valores y observa los resultados con let y const.
+let ciudad = "buenos aires"
+let pais = "argentina"
+let edad = 20 
+console.log(ciudad.pais.edad)
